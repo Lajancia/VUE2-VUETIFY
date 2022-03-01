3f07c655c60f0d8c90c879dc21ba53ad673922a0
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-03-01 16:35:57 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-app-bar app color="black" dark>
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div></v-app-bar
+      ></v-layout
+    >
+    <v-layout>
+      <router-view></router-view>
+    </v-layout>
+  </v-container>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
