41cb852d48311b37ee117cfd25dba15c4cd69e29
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-03-05 16:27:12 +0900

    favicon, webpack add

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,15 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  configureWebpack: config => {
+    
+    if (process.env.NODE_ENV === 'production') {
+        config.mode = 'production';
+                     // Package file size configuration
+        config.performance = {
+          maxEntrypointSize: 10000000,
+          maxAssetSize: 30000000
+        }
+    }
+},
 }
